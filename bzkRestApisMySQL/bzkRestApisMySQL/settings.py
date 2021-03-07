ab18bd5a81d939e53dafe9155c8186f52dd6b338
Pratik Pugalia <pratik.pugalia@phonepe.com>
2021-03-08 02:58:21 +0530

    Dockerise setup

--- bzkRestApisMySQL/bzkRestApisMySQL/settings.py
+++ bzkRestApisMySQL/bzkRestApisMySQL/settings.py
@@ -92,7 +92,7 @@ DATABASES = {
         'NAME': 'testdb',
         'USER': 'root',
         'PASSWORD': '123456',
-        'HOST': '127.0.0.1',
+        'HOST': 'db',
         'PORT': '3306',
     }
 }
